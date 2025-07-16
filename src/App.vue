68880b090183a9b6581e62d16dca42797806bc7f
Joyline Karanja <joylinekaranja@Joylines-MacBook-Pro.local>
2025-07-16 16:58:18 +0300

    Adding a functionality to add and remove todo activities

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,61 @@ import { reactive, ref } from 'vue'
 // const counter = reactive({ count: 0 })
 // const message = ref('Hello World!')
 
-const tileColour = ref('titlecolor')
-const count = ref(0)
-function increment() {
-  // update component state
-  count.value++
-}
-//the toggle feature for the button, mwehehe
-const awesome = ref(true)
+// const tileColour = ref('titlecolor')
+// const count = ref(0)
+// function increment() {
+//   // update component state
+//   count.value++
+// }
+// //the toggle feature for the button, mwehehe
+// const awesome = ref(true)
 // function toggle(){
 
 // }
+//calling the list view with mutating methods, first give them unique ids
+let id=0
+const newTodo = ref('')
+const todos = ref()[
+  {id: id++, text: 'Learn Html'},
+  {id: id++, text:'Learn Javascript'},
+  {id: id++, text:'Learn Vue'}
+]
 
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
 
-  <button @click="awesome = !awesome">Toggle</button>
+  <!-- <button @click="awesome = !awesome">Toggle</button>
   <h1 v-if="awesome" >Vue is awesome!</h1>
-  <h1 v-else>Oh brother🥴</h1>
+  <h1 v-else>Oh brother🥴</h1> -->
 
-  <h2 :class="tileColour"> I am a certain color</h2>
+  <!-- <h2 :class="tileColour"> I am a certain color</h2> -->
   <!-- button  -->
-  <button @click="increment">
-    On click count is {{ count }}
-  </button>
+  <!-- <button @click="increment">
+    On click count is {{ count }} -->
+  <!-- </button> -->
   <!-- input section is here -->
-   <input v-model="text">
-   <p>{{ text }}</p>
+   <!-- <input v-model="text">
+   <p>{{ text }}</p> -->
+
+   <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
 
 <style>
